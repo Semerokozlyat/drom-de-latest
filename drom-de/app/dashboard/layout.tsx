d54bcbada89773ba6f77c99a9b561a00646e7fd5
evgeniy.pomortsev <evgeniy.pomortsev@acronis.com>
2024-11-05 18:45:49 +0100

    Add dashboard and links

---
+++ drom-de/app/dashboard/layout.tsx
@@ -0,0 +1,15 @@
+import SideNav from '@/app/ui/dashboard/sidenav';
+import React from "react";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="flex flex-col md:flex-row md:overflow-hidden h-screen">
+            <div className="w-full flex-none md:w-64">
+                <SideNav />
+            </div>
+            <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+                {children}  {/* All underlying page.tsx files will be rendered here */}
+            </div>
+        </div>
+    );
+}
