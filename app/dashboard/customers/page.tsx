ffcdc9e14d20954675ac02d92bbdc1fde9d7ead3
evgeniy.pomortsev <evgeniy.pomortsev@acronis.com>
2024-11-18 16:49:35 +0100

    Add website metadata: page titles, description, icons

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,8 @@
+import {Metadata} from "next";
 
+export const metadata: Metadata = {
+    title: 'Invoices',  // this title will be set to the template placeholder inside /app/layout.tsx
+}
 
 export default function Page() {
     return <p>Customers Page</p>;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 import 'app/ui/global.css';
 import { inter } from 'app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: {
+        template: '%s | Drom de dashboard',
+        default: 'Drom de dashboard'
+    },
+    description: 'Web portal for auto reviews and private rentals',
+    metadataBase: new URL('https://drom.de/about'),
+}
 
 export default function RootLayout({
   children,
